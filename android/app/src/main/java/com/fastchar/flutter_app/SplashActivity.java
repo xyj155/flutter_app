4d5f86b52871a06520976754998708ad0ae56d33
xyj155 <1789780841@qq.com>
2019-08-21 23:32:23 +0800

    Initial commit

---
+++ android/app/src/main/java/com/fastchar/flutter_app/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.fastchar.flutter_app;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        }, 3000);
+    }
+}
---
+++ lib/entity_factory.dart
@@ -0,0 +1,19 @@
+import 'gson/home_title_avatar_entity.dart';
+import 'gson/home_user_topic_entity.dart';
+import 'gson/user_post_item_entity.dart';
+
+class EntityFactory {
+  static T generateOBJ<T>(json) {
+    if (1 == 0) {
+      return null;
+    } else if (T.toString() == "HomeTitleAvatarEntity") {
+      return HomeTitleAvatarEntity.fromJson(json) as T;
+    } else if (T.toString() == "HomeUserTopicEntity") {
+      return HomeUserTopicEntity.fromJson(json) as T;
+    } else if (T.toString() == "UserPostItemEntity") {
+      return UserPostItemEntity.fromJson(json) as T;
+    } else {
+      return null;
+    }
+  }
+}
